b3d08372ffd4b3aad3a714b74ce75c30625a364b
zongjiayang <845944350@qq.com>
2023-06-17 12:11:14 +0800

    初始化项目

---
+++ src/main/java/com/zjy/zflow/ZflowApplication.java
@@ -0,0 +1,13 @@
+package com.zjy.zflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZflowApplication.class, args);
+    }
+
+}
